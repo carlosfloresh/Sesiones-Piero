94e2c8008bc8ec09963c867f75d393a39fc8873e
losii <u202513368@upc.edu.pe>
2026-03-08 20:39:13 -0500

    hola

--- 01-03/Practice/Arr_Puntero_ej03.cpp
+++ 01-03/Practice/Arr_Puntero_ej03.cpp
@@ -9,6 +9,23 @@
 using namespace std;
 int main()
 {
-
+    int n;
+    cout << "Ingrese una cantidad de numbers para su arrays: " << endl;
+    cin >> n;
+    int* notas = new int[n];
+    cout << "A continuacion, ingresa la nota que sacaste en cada curso." << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Nota en el curso " << i + 1 << ": ";
+        cin >> notas[i];
+    }
+    int mayor = notas[0], menor = notas[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (notas[i] > mayor) mayor = notas[i];
+        if (notas[i] < menor) menor = notas[i];
+    }
+    cout << mayor << endl;
+    cout << menor;
     return 0;
 }
