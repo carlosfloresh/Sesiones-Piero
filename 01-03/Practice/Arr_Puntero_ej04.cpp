27c4980b925d1f52321b0fccd83478c58e427c6a
losii <u202513368@upc.edu.pe>
2026-03-08 21:16:26 -0500

    ksdkskd

--- 01-03/Practice/Arr_Puntero_ej04.cpp
+++ 01-03/Practice/Arr_Puntero_ej04.cpp
@@ -12,27 +12,25 @@ int main()
     int n;
     cout << "Ingrese una cantidad de numbers para su arrays: " << endl;
     cin >> n;
+    int positivos = 0;
     int* notas = new int[n];
     cout << "A continuacion, ingresa la nota que sacaste en cada curso." << endl;
     for (int i = 0; i < n; i++)
     {
         cout << "Nota en el curso " << i + 1 << ": ";
         cin >> notas[i];
+        if (notas[i] > 0) positivos++;
     }
     for (int i = 0; i < n; i++) {
         cout << notas[i] << " ";
     }
     cout << endl;
-    int* nuevoArray = new int[n];
+    int nuevoArray[positivos];
     for (int i = 0; i < n; i++) {
         if (notas[i] >= 0) nuevoArray[i] = notas[i];
-        else {
-            n -= 1;
-            continue;
-        }
     }
     cout << "Tu nuevo array con solo positivos es: " << endl;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < positivos; i++) {
         cout << nuevoArray[i] << " ";
     }
     return 0;
