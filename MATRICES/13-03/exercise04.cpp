0fb978abb463bb666d5c138a3c0c69bc0bc2de3c
carlosfloresh <cfloresh99@gmail.com>
2026-03-13 21:55:22 -0500

    add excersice 04

---
+++ MATRICES/13-03/exercise04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// Decirle al usuario ingrese filas y columnas ---> crear esa matriz dinamicamente y rellenar e imprimirlo.
+// Iniciar un matriz de forma dinamica... [a][b]
+// Iniciar un matriz de forma dinamica... [a][b]
+
+// Iniciar un mat+mat2 de forma dinamica...
+// Iniciar un matriz de forma dinamica... [2a][2b]
+// Imprimir el size y los elementos
+
+int main() {
+
+    return 0;
+}
