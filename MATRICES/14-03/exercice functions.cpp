6b3a615125dd0f77651cbf7b340e70f4b79a8576
carlosfloresh <cfloresh99@gmail.com>
2026-03-14 19:09:51 -0500

    function excersice

---
+++ MATRICES/14-03/exercice functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// Size
+void sizeMat(int *rows, int *cols) {
+    cout << "Enter the size of rows: " << endl; cin >> *rows;
+    cout << "Enter the size of cols: " << endl; cin >> *cols;
+}
+
+// Create matriz
+
+// Insert data into matriz
+// Print
+
+int main()
+{
+    int rows = 0, cols = 0;
+    sizeMat(&rows, &cols);
+
+
+
+
+    return 0;
+}
